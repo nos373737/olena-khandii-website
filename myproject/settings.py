5f8e1fc7916d5b7ef34d4dfbcb4d8e046c52b3b1
Andrii Nos <annos@cisco.com>
2025-07-29 12:27:20 +0300

    nginx, cloud-config, certbot

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,13 @@ SECRET_KEY = "django-insecure-s0eg9q5^i%wy4n5m78yvbpwkb3j3u)w8nx=pr&c6+ag8ltvs5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'olena-english.com',
+    'www.olena-english.com', 
+    '136.244.84.166',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
